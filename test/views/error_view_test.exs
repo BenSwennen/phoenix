567a954ab14645541e9166b9cc8c656741f7d552
Ben Swennen <b_swennen@hotmail.com>
2016-07-14 06:45:56 +0200

    Add json error views

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -1,21 +1,30 @@
 defmodule Rialto.ErrorViewTest do
   use Rialto.ConnCase, async: true
 
-  # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
+
+  test "renders 401.json" do
+    assert render(Rialto.ErrorView, "401.json", []) ==
+           %{errors: [%{code: 401, title: "Unauthorized"}]}
+  end
+
+  test "renders 403.json" do
+    assert render(Rialto.ErrorView, "403.json", []) ==
+           %{errors: [%{code: 403, title: "Forbidden"}]}
+  end
 
   test "renders 404.json" do
     assert render(Rialto.ErrorView, "404.json", []) ==
-           %{errors: %{detail: "Page not found"}}
+           %{errors: [%{code: 404, title: "Not Found"}]}
   end
 
   test "render 500.json" do
     assert render(Rialto.ErrorView, "500.json", []) ==
-           %{errors: %{detail: "Internal server error"}}
+           %{errors: [%{code: 500, title: "Internal Server Error"}]}
   end
 
   test "render any other" do
     assert render(Rialto.ErrorView, "505.json", []) ==
-           %{errors: %{detail: "Internal server error"}}
+           %{errors: [%{code: 500, title: "Internal Server Error"}]}
   end
 end
